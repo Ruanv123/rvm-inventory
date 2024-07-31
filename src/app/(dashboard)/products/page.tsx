b19c7d8aad7bcb69c89b243495de60dbe77eaace
Ruan Victor <ruanvictormr@gmail.com>
2024-07-31 01:47:03 -0300

    refactor search filter

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -21,7 +21,6 @@ import { EllipsisVertical, PlusCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { DeleteButton } from "./_components/deleteButton";
-import { SeachFilter } from "./_components/filter";
 
 export default async function ProductPage({
   searchParams,
@@ -61,7 +60,6 @@ export default async function ProductPage({
       {data.length > 0 ? (
         <section className="grid gap-2 items-end">
           <div className="flex">
-            <SeachFilter />
             <Pagination currentPage={currentPage} totalPages={totalPages} />
           </div>
           <div className="overflow-hidden rounded-md border">
