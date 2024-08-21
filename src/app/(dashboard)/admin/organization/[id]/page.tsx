6b1665a7084b59f9c4ff27d444aa5b8310f38793
Ruan Victor <ruanvictormr@gmail.com>
2024-08-20 23:33:51 -0300

    feat: users table on organization page

--- src/app/(dashboard)/admin/organization/[id]/page.tsx
+++ src/app/(dashboard)/admin/organization/[id]/page.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
+import { Loader } from "@/components/shared/loader";
+import { UserTable } from "@/components/tables/organization/user-table";
 import prisma from "@/lib/db";
+import { Suspense } from "react";
 
 export default async function OrganizationDetail({
   params,
@@ -9,6 +12,13 @@ export default async function OrganizationDetail({
   const organization = await prisma.organization.findUnique({
     where: { id: +params.id },
   });
+
+  const users = await prisma.user.findMany({
+    where: { organizationId: +params.id },
+    skip: 0,
+    take: 5,
+  });
+
   return (
     <>
       <h1>Organization: {organization?.name}</h1>
@@ -23,8 +33,20 @@ export default async function OrganizationDetail({
         />
         <div className="grid items-start">
           <h3>{organization?.name}</h3>
-          {/* <p>{organization?.description}</p> */}
+          <p>{organization?.description}</p>
         </div>
+      </div>
+
+      <div className="mt-5 w-full">
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center h-full">
+              <Loader />
+            </div>
+          }
+        >
+          <UserTable user={users} />
+        </Suspense>
       </div>
     </>
   );
